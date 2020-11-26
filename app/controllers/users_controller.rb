39fa55ad0d81310f1ad1d05a10b2334adec89429
Kaneko-Yukihiro0117 <74887355+Kaneko-Yukihiro0117@users.noreply.github.com>
2020-11-26 16:06:21 +0900

    バリデーションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
  
-  def edit
+  def edit 
     end
   def update
     if current_user.update(user_params)
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     end
   end
   
-  
+
   private
 
   def user_params
